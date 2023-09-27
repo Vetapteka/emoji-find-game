1b1c569ed0ec136dc92081d2a552567e42ddb404
vetapteka <cvetka_top@mail.ru>
2023-09-27 22:24:21 +0300

    KEYBOARD: Can select emoji

--- src/pages/game-page/keyboard/Keyboard.tsx
+++ src/pages/game-page/keyboard/Keyboard.tsx
@@ -1,14 +1,15 @@
-import React, { useState } from 'react';
-import { EmojiListKeys } from '../../../store/simple-emoji/simpleEmoji.model';
-import styled from 'styled-components';
-import { emojiCategories } from '../../../data/emoji';
-import { TStateSetter } from '../../../types';
 // @ts-ignore
 import keyboard_container from '../../../assets/containers/keyboard.svg';
+import React, { useState } from 'react';
+import { EmojiListKeys } from '../../../store/simple-emoji/emoji.model';
+import styled from 'styled-components';
+import { emojiCategories } from '../../../data/emojiCodes';
+import { TStateSetter } from '../../../types';
 import { Keys } from './Keys';
 import Switches from './switcher/Switcher';
-import { useAppSelector } from '../../../hooks/redux';
-import { ISimpleEmojiState } from '../../../store/simple-emoji/simpleEmoji.sllice';
+import { useAppDispatch, useAppSelector } from '../../../hooks/redux';
+import { IEmojiState } from '../../../store/simple-emoji/emoji.sllice';
+import { setSelectedEmoji } from '../../../store/emoji-mapper/emojiMapper.slice';
 
 const Wrapper = styled.div`
   height: 100%;
@@ -19,14 +20,17 @@ const Wrapper = styled.div`
 `;
 
 function Keyboard(): JSX.Element {
-	const { emojiList }: ISimpleEmojiState = useAppSelector(state => state.simpleEmojiReducer);
+	const dispatch = useAppDispatch();
+	const { emojiList }: IEmojiState = useAppSelector(state => state.simpleEmojiReducer);
 	const [panel, setPanel]: [EmojiListKeys, TStateSetter<EmojiListKeys>] = useState<EmojiListKeys>('yellowList');
+
 	return (
 		<Wrapper>
 			<Switches setPanel={setPanel}/>
 			<Keys>
 				{emojiCategories[panel].map((code: string) =>
 					<img
+						onClick={() => dispatch(setSelectedEmoji(code))}
 						loading="lazy"
 						alt={code}
 						src={`data:image/svg+xml;utf8,${encodeURIComponent(emojiList[code])}`}
