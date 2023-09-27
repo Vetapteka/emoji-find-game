917a2724ab73b71fb7568a4b741635cf1d4b7af3
vetapteka <cvetka_top@mail.ru>
2023-09-27 22:25:48 +0300

    GAME_PAGE: (beta) Add buttons for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -9,20 +9,26 @@ import GamePage from './pages/game-page/GamePage';
 const store = setupStore();
 
 function App() {
-	const [page, setPage]: TPageContext = useState<Pages>(Pages.START_PAGE);
+	const [page, setPage]: TOpenedPageContext = useState<Pages>(Pages.START_PAGE);
 
 	return (
 		<Provider store={store}>
-			<PageContext.Provider value={[page, setPage]}>
-				<button style={{position: 'absolute', top: '0', left: '0'}} onClick={() => setPage(Pages.START_PAGE)}>start</button>
-				<button style={{position: 'absolute', top: '70px', left: '0'}} onClick={() => setPage(Pages.MENU_PAGE)}>menu</button>
-				<button style={{position: 'absolute', top: '100px', left: '0'}} onClick={() => setPage(Pages.GAME_PAGE)}>game</button>
+			<OpenedPageContext.Provider value={[page, setPage]}>
+				<button style={{ position: 'absolute', top: '0', left: '0' }}
+						onClick={() => setPage(Pages.START_PAGE)}>start
+				</button>
+				<button style={{ position: 'absolute', top: '70px', left: '0' }}
+						onClick={() => setPage(Pages.MENU_PAGE)}>menu
+				</button>
+				<button style={{ position: 'absolute', top: '100px', left: '0' }}
+						onClick={() => setPage(Pages.GAME_PAGE)}>game
+				</button>
 
-					{page === Pages.START_PAGE && <StartPage/>}
-					{page === Pages.MENU_PAGE && <MenuPage/>}
-					{page === Pages.GAME_PAGE && <GamePage/>}
+				{page === Pages.START_PAGE && <StartPage/>}
+				{page === Pages.MENU_PAGE && <MenuPage/>}
+				{page === Pages.GAME_PAGE && <GamePage/>}
 
-			</PageContext.Provider>
+			</OpenedPageContext.Provider>
 		</Provider>
 	);
 }
