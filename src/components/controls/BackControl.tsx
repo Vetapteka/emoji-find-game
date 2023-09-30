43d76c5f96f62919e43852c3d3f9f306ca988d9d
vetapteka <cvetka_top@mail.ru>
2023-10-01 00:59:45 +0300

    COMPONENTS: Add BackControl

---
+++ src/components/controls/BackControl.tsx
@@ -0,0 +1,15 @@
+import React, { JSX } from 'react';
+import Icon, { IconSizeEnum } from '../icons/Icon';
+import { AppDispatch } from '../../store/store';
+import { useAppDispatch } from '../../hooks/redux';
+import { openPreviousPage } from '../../store/pages/pages.slice';
+
+function BackControl(): JSX.Element {
+	const dispatch: AppDispatch = useAppDispatch();
+	const openPreviousPageHandler = () => dispatch(openPreviousPage());
+	return (
+		<Icon icon="BACK_ICON" size={IconSizeEnum.S} onClick={openPreviousPageHandler}/>
+	);
+}
+
+export default BackControl;
