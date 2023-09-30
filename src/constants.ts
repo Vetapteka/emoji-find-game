64096368441fd889c067e242245dedf3b7ef3e20
vetapteka <cvetka_top@mail.ru>
2023-10-01 00:58:54 +0300

    GAME: Add fonts, styles, constants, change cursor

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export const KEYBOARD_HEIGHT = '35vh';
+
+export const ACTIVE_ICON_COLOR = 'white';
+
+export const SELECTED_EMOJI_CONTAINER_SIZE = '12vh';
+
+export const TARGET_EMOJI_CONTAINER_SIZE = '15vh';
+
+export const MERGE_EMOJI_SIZE = '10vh';
