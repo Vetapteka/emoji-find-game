fcec7838a6c7276fcde74f3f1bb3a7b02eb573cf
vetapteka <cvetka_top@mail.ru>
2023-10-01 00:49:24 +0300

    STORE_USER: Add user store

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,13 +2,16 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import emojiReducer from './emoji/emoji.sllice';
 import emojiMapperReducer from './emoji-mapper/emojiMapper.slice';
 import timeReducer from './timer/timer.slice';
+import pagesReducer from './pages/pages.slice';
+import userReducer from './user/user.slice';
 
 export const rootReducer = combineReducers({
 	emojiReducer,
 	emojiMapperReducer,
-	timeReducer
+	timeReducer,
+	pagesReducer,
+	userReducer
 });
-
 
 export const setupStore = () => {
 	return configureStore({
@@ -16,7 +19,6 @@ export const setupStore = () => {
 	});
 
 };
-
 
 export type RootState = ReturnType<typeof rootReducer>
 export type AppStore = ReturnType<typeof setupStore>
