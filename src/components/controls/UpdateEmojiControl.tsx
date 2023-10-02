34c0dcfc57fa6e932ad3c48eb84a0e9cad86908f
vetapteka <cvetka_top@mail.ru>
2023-10-02 17:39:26 +0300

    GAME: Add UpdateEmojiControl

---
+++ src/components/controls/UpdateEmojiControl.tsx
@@ -0,0 +1,10 @@
+import React, { JSX } from 'react';
+import Icon, { IconSizeEnum } from '../icons/Icon';
+
+function UpdateEmojiControl(): JSX.Element {
+	return (
+		<Icon icon={'UPDATE_ICON'} size={IconSizeEnum.XS}/>
+	);
+}
+
+export default UpdateEmojiControl;
