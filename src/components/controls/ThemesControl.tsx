7853fcc5151115dceece04748e4f96d32f5090ae
vetapteka <cvetka_top@mail.ru>
2023-10-02 17:40:22 +0300

    GAME_PAGE: Add ThemesControl

---
+++ src/components/controls/ThemesControl.tsx
@@ -0,0 +1,10 @@
+import React, { JSX } from 'react';
+import Icon, { IconSizeEnum } from '../icons/Icon';
+
+function ThemesControl(): JSX.Element {
+	return (
+		<Icon icon="THEME_ICON" size={IconSizeEnum.L}/>
+	);
+}
+
+export default ThemesControl
