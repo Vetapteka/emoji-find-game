95d51c721fc19ab9fb6ff759109e1e92a8425deb
vetapteka <cvetka_top@mail.ru>
2023-10-02 17:42:02 +0300

    GAME_PAGE: Add ReplayControl

---
+++ src/components/controls/ReplayControl.tsx
@@ -0,0 +1,10 @@
+import React, { JSX } from 'react';
+import Icon, { IconSizeEnum } from '../icons/Icon';
+
+function ReplayControl(): JSX.Element {
+	return (
+		<Icon icon="REPLAY_ICON" size={IconSizeEnum.M}/>
+	);
+}
+
+export default ReplayControl
