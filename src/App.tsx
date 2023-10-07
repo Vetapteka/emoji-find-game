e8f16fec53fc7a218c74b37e1fdbf11cc2936ff5
vetapteka <cvetka_top@mail.ru>
2023-10-07 22:19:50 +0300

    GAME: Add Spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import React, { JSX, Suspense } from 'react';
 import { useAppSelector } from './hooks/redux';
 import { IPagesState } from './store/pages/pages.model';
-import PauseModal from './modals/PauseModal';
 import { RootState } from './store/store';
+import Spinner from './components/Spinner';
 
 const StartPage = React.lazy(() => import('./pages/StartPage'));
 const GamePage = React.lazy(() => import('./pages/GamePage'));
+const PauseModal = React.lazy(() => import('./modals/PauseModal'));
+// const CountdownModal = React.lazy(() => import('./modals/CountdownModal'));
 
 function App(): JSX.Element {
 	const { openedPage, openedModal }: IPagesState = useAppSelector((state: RootState) => state.pagesReducer);
 
 	return (
 		<>
-			{openedPage === 'START_PAGE' && <Suspense fallback={'loading.....'}><StartPage/></Suspense>}
-			{openedPage === 'GAME_PAGE' && <Suspense fallback={'loading.....'}><GamePage/></Suspense>}
-			{openedModal === 'PAUSE_MODAL' && <Suspense fallback={'loading.....'}><PauseModal/></Suspense>}
+			{openedPage === 'START_PAGE' && <Suspense fallback={<Spinner/>}><StartPage/></Suspense>}
+			{openedPage === 'GAME_PAGE' && <Suspense fallback={<Spinner/>}><GamePage/></Suspense>}
+			{openedModal === 'PAUSE_MODAL' && <Suspense fallback={<Spinner/>}><PauseModal/></Suspense>}
+			{/*{openedModal === 'COUNTDOWN_MODAL' && <Suspense fallback={<Spinner/>}><CountdownModal/></Suspense>}*/}
 		</>
 	);
 }
