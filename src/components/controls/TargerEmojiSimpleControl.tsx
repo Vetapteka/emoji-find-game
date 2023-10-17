b149cc8f825c67090a947e6bb9a071763f9df5ca
vetapteka <cvetka_top@mail.ru>
2023-10-17 16:59:02 +0300

    GAME_PAGE: Refactor TargetEmojiSimpleControl

---
+++ src/components/controls/TargerEmojiSimpleControl.tsx
@@ -0,0 +1,24 @@
+import React, { JSX } from 'react';
+import { googleRequestUrl } from '../../store/emoji-mapper/emojiUtils';
+import { useAppSelector } from '../../hooks/redux';
+import { RootState } from '../../store/store';
+import { IEmojiMapperState } from '../../store/emoji-mapper/emojiMapper.slice';
+
+interface TargetEmojiSimpleControlProps {
+	className?: string;
+}
+
+function TargetEmojiSimpleControl({ ...props }: TargetEmojiSimpleControlProps): JSX.Element {
+	const { targetEmoji }: IEmojiMapperState = useAppSelector((state: RootState) => state.emojiMapperReducer);
+
+	return (
+		<>{targetEmoji && <img
+			{...props}
+			loading="eager"
+			alt="target_emoji"
+			src={googleRequestUrl(targetEmoji)}/>
+		}</>
+	);
+}
+
+export default TargetEmojiSimpleControl;
--- src/components/controls/TargetEmojiControl.tsx
+++ src/components/controls/TargetEmojiControl.tsx
@@ -1,36 +1,26 @@
 import React, { JSX } from 'react';
 import { TARGET_EMOJI_CONTAINER_SIZE } from '../../constants';
-import { googleRequestUrl } from '../../store/emoji-mapper/emojiUtils';
 import PenBorder from '../borders/PenBorder';
-import { useAppSelector } from '../../hooks/redux';
-import { RootState } from '../../store/store';
-import { IEmojiMapperState } from '../../store/emoji-mapper/emojiMapper.slice';
 import styled from 'styled-components';
 import UpdateEmojiControl from './UpdateEmojiControl';
+import TargetEmojiSimpleControl from './TargerEmojiSimpleControl';
 
 const Wrapper = styled.div`
   position: relative;
 
   & > .update {
     position: absolute;
-	top: -0.7vh;
-	right: -1.5vh;
+    top: -0.7vh;
+    right: -1.5vh;
   }
 `;
 
 function TargetEmojiControl(): JSX.Element {
-	const { targetEmoji }: IEmojiMapperState = useAppSelector((state: RootState) => state.emojiMapperReducer);
-
 	return (
 		<Wrapper>
 			<UpdateEmojiControl className="update"/>
 			<PenBorder size={TARGET_EMOJI_CONTAINER_SIZE}>
-				{targetEmoji &&
-					<img
-						loading="eager"
-						alt="target_emoji"
-						src={googleRequestUrl(targetEmoji)}
-					/>}
+				<TargetEmojiSimpleControl/>
 			</PenBorder>
 		</Wrapper>
 	);
